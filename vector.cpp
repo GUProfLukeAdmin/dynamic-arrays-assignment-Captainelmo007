d6d6b393ad9607b7391b68087a75188ca126be80
CaptainElmo007 <tsnycerski@outlook.com>
2025-02-09 14:03:26 -0800

    Done

--- vector.cpp
+++ vector.cpp
@@ -1,14 +1,33 @@
 #include <vector>
 #include <iostream>
 
+using namespace std;
 
 void part1() {
-    //TODO: Implement part 1
+    std::vector<int> v;
+
+    for(int i = 0; i < 50000; i++) {
+        v.push_back(i);
+    }
+    
+    std::cout << "After adding elements: Size = " << v.size() << ", Capacity = " << v.capacity() << "\n";
+
+
+    v.resize(v.size() / 2);
+    std::cout << "After resizing: Size = " << v.size() << ", Capacity = " << v.capacity() << "\n";
+
+
+    v.shrink_to_fit();
+    std::cout << "After shrink_to_fit(): Size = " << v.size() << ", Capacity = " << v.capacity() << "\n";
 }
 
 void part2() {
-    //TODO: Implement part 2
-    std::cout << "Testing Github" << std::endl;
+    std::vector<bool> boolVector(10000);
+    std::vector<char> charVector(10000);
+
+
+    std::cout << "Memory usage of std::vector<bool>: " << (boolVector.capacity() / 8 * sizeof(bool)) << " bytes" << std::endl;
+    std::cout << "Memory usage of std::vector<char>: " << (charVector.capacity() * sizeof(char)) << " bytes" << std::endl;
 }
 
 int main() {
